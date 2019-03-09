003776dc223027f7f5963686a7b591db31aad064
shashank <dev.py.sha@gmail.com>
2019-03-09 13:45:31 +0530

    build success :initial Commit :)

---
+++ precache-manifest.72d6996a3f5ad0ad6cadef8d69d1fc31.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ef4a61e4900d476a7b8f7b6390ab1eb",
+    "url": "/static/media/fish-gang.1ef4a61e.png"
+  },
+  {
+    "revision": "bf7be418715a51776b470cecbc542a69",
+    "url": "/static/media/fish-doodle.bf7be418.png"
+  },
+  {
+    "revision": "f7e92cd78d1a6a7051d9b0da60f6836d",
+    "url": "/static/media/coral-lower.f7e92cd7.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "89d81753f5f09991d2c3",
+    "url": "/static/js/main.89d81753.chunk.js"
+  },
+  {
+    "revision": "5a76c85a028d0163ec21",
+    "url": "/static/js/2.5a76c85a.chunk.js"
+  },
+  {
+    "revision": "89d81753f5f09991d2c3",
+    "url": "/static/css/main.730c003b.chunk.css"
+  },
+  {
+    "revision": "2d5d419e394c6463f5ebd140e586f2df",
+    "url": "/index.html"
+  }
+];
